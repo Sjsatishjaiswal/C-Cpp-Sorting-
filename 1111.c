851957703d9f0b608a1a359cc997b29d97cee46b
Satish kumar Jaiswal <45370567+Sjsatishjaiswal@users.noreply.github.com>
2020-09-16 04:51:53 +0530

    BCE book

---
+++ 1111.c
@@ -0,0 +1,14 @@
+int main()
+{
+    int r,c;
+    for(r=4;r>=1;r--)
+    {printf("$");
+        for(c=1;c<=r;c++)
+            printf("%ul\t" ,c);
+            printf("jaiswal");
+
+ printf("\n");
+
+    }
+    printf("** SATISH JAISWAL**");
+}

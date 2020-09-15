103f523e66120594954768998b16e7698ebe79af
Satish kumar Jaiswal <45370567+Sjsatishjaiswal@users.noreply.github.com>
2020-09-16 05:03:39 +0530

    star pattern program

---
+++ star patter 3 - Copy.c
@@ -0,0 +1,16 @@
+int main()
+{
+    int i,j,k;
+    for (i=1;i=7;i++)
+    {
+        for(j=1;j<=7;j++)
+        {
+            if()
+                printf("*");
+            else
+                printf ("\n");
+        }
+    }
+
+
+}
